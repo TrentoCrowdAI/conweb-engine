bb92a80b9e8c40ae863954210d198ca4c2a1df28
Marcos Baez <mbaezpy@gmail.com>
2019-03-13 22:00:26 +0100

    cleaned up the package entry point

--- app.js
+++ app.js
@@ -1,29 +1,9 @@
 const puppeteer = require('puppeteer');
-
 const engine = require('./components/engine');
 
-/*jquery library to be loaded
-start with lists of proposals
-input: javascript object with:
-- intent (what the user wants to do, eg: list_resources)
-- component (what I'm working with)
-- resource (which specific list, eg: proposals)
-- selector (specific is, eg: ul.list)
-to be searched with specific bot-attribute tags OR, if they're standard, with their standard attribute e.g. h1, etc
-then it returns a .json with attribue:value pairs, for now only on lists, but we need to think about other components like for forms
-include a snapshot of how the list looks in the browser for every interaction. Convert the -bmp to base64 so that we don't store them, but just render when requested
-<mimetype> <base64_encoed_image>*/
 
-var url;
 
-if (process.argv.length < 3) {
-  console.log("Usage:   node index.js url");
-  console.log("Example: node index.js https://nodejs.org/api/url.html");
-} else {
-  url = process.argv[2];
-}
-
-var processIntent = async (request) => {
+exports.processIntent = async (request) => {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
@@ -44,35 +24,3 @@ var processIntent = async (request) => {
 
   return resp;
 };
-
-
-var request = {
-  url: "http://localhost:8080/static/example.html",
-  component: "list",  
-  query: { 
-    intent : "list_resources",
-    resource : {
-      name : "movies",
-      selector : "ul",
-      attributes : [{
-        name : "title",
-        selector : "h1"
-      },{
-        name : "stars",
-        selector : "[bot-attribute=stars]"
-      },{
-        name : "plot",
-        selector : "[bot-attribute=plot]"
-      }]
-    }
-  }
-};
-
-
-try {
-  processIntent(request).then(res => console.log(res));
-} catch (err) {
-  
-  console.log("Error========");
-  console.log(err);
-}
